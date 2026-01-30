8ac1816cad9571133a84cac35b9c84279ddded14
SBabchukTM <babchuk.serhii@trident-media.agency>
2026-01-30 14:31:43 +0200

    init

---
+++ EggyRushTime/App/EggyRushTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggyRushTimeApp.swift
+//  EggyRushTime
+//
+//  Created by Serhii Babchuk on 30.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct EggyRushTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ERLaunchView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-EggyRushTime/Pods-EggyRushTime-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EggyRushTime : NSObject
+@end
+@implementation PodsDummy_Pods_EggyRushTime
+@end
